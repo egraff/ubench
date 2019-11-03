773da1948f6920e15d3ae43919bf7e9698770c1d
Erlend Graff <erlend.graff@gmail.com>
2019-11-03 19:07:59 +0100

    clocks: new implementation of x86 RDTSC-based cycle clock

--- src/clocks/cpu_arch_clock.c
+++ src/clocks/cpu_arch_clock.c
@@ -2,111 +2,113 @@
 #include "ubench_arch_def.h"
 #include "ubench_compiler.h"
 
+#if defined(__i386__) || defined(__x86_64__)
+  #include "ubench_intrin_x86.h"
+#endif /* defined(__i386__) || defined(__x86_64__) */
+
 #include <ubench/ubench_time.h>
 #include <ubench/ubench.h>
 #include <stdint.h>
 
 
-#if defined(_MSC_VER)
-  #include <intrin.h>
-#endif /* defined(_MSC_VER) */
-
-
-#if defined(__GNUC__)
-  #if defined(__i386__) || defined(__x86_64__)
-    #include <cpuid.h>
-  #endif /* defined(__i386__) || defined(__x86_64__) */
-#endif /* defined(__GNUC__) */
-
-
-#if defined(__GNUC__) && defined(__INTEL_COMPILER)
-  #if defined(__i386__) || defined(__x86_64__)
-    #include <immintrin.h>
-  #endif /* defined(__i386__) || defined(__x86_64__) */
-#endif /* defined(__GNUC__) && defined(__INTEL_COMPILER) */
-
-
-#if defined(__GNUC__) && !defined(__INTEL_COMPILER)
-  #if defined(__i386__) || defined(__x86_64__)
-    #include <x86intrin.h>
-  #endif /* defined(__i386__) || defined(__x86_64__) */
-#endif /* defined(__GNUC__) && !defined(__INTEL_COMPILER) */
-
-
-#if defined(__GNUC__) && !defined(__clang__)
-  #if !GNUC_PREREQ(4, 5)
-static FORCE_INLINE int64_t
-__rdtsc()
-{
-    uint32_t eax, edx;
-    uint64_t res;
-
-    __asm__ __volatile__("rdtsc" : "=a" (eax), "=d" (edx));
-    res = ((uint64_t)edx << 32) | eax;
-    return *((int64_t *)&res);
-}
-
-
-static FORCE_INLINE int64_t
-__rdtscp(uint32_t *aux)
-{
-    uint32_t eax, ecx, edx;
-    uint64_t res;
-
-    __asm__ __volatile__("rdtscp" : "=a" (eax), "=d" (edx), "=c" (ecx));
-    res = ((uint64_t)edx << 32) | eax;
-    *aux = ecx;
-    return *((int64_t *)&res);
-}
-  #endif /* !GNUC_PREREQ(4, 5) */
-#endif /* defined(__GNUC__) && !defined(__clang__) */
-
-
 #if defined(__i386__) || defined(__x86_64__)
 
+/* Regarding the use of RDTSCP:
+ * ----------------------------
+ *
+ * Intel SDM Volume 2B (https://software.intel.com/sites/default/files/managed/7c/f1/253667-sdm-vol-2b.pdf)
+ * states that
+ *
+ *   [RDTSCP] is not a serializing instruction, but it does wait until all
+ *   previous instructions have executed and all previous loads are globally
+ *   visible. But it does not wait for previous stores to be globally visible,
+ *   and subsequent instructions may begin execution before the read operation
+ *   is performed
+ *
+ * and
+ *
+ *   If software requires RDTSCP to be executed prior to execution of any
+ *   subsequent instruction (including any memory accesses), it can execute
+ *   LFENCE immediately after RDTSCP.
+ *
+ * On the other hand, the AMD APM Volume 2 (https://www.amd.com/system/files/TechDocs/24593.pdf)
+ * states somewhat vaguely that
+ *
+ *   The RDTSC instruction (unlike the RDTSCP instruction) is not serializing,
+ *
+ * but RDTSCP is not listed as one of the serializing non-privileged
+ * instructions on AMD64 processors (CPUID, IRET, RSM, MFENCE). However, the
+ * AMD white paper "Software Techniques For Managing Speculation on AMD
+ * Processors" (https://developer.amd.com/wp-content/resources/Managing-Speculation-on-AMD-Processors.pdf)
+ * clarifies that
+ *
+ *   [RDTSCP] is also dispatch serializing.
+ *
+ * This means that when using RDTSCP at the end of a measured region, we do
+ * not need a preceding LFENCE, but we do need a subsequent LFENCE immediately
+ * following the RDTSCP.
+ */
+
+
 static FORCE_INLINE void
-cpuid(uint32_t leaf, uint32_t cpu_info[4])
-{
-#if defined(_MSC_VER)
-    __cpuid((int *)cpu_info, leaf);
-#elif defined(__GNUC__)
-    __cpuid(leaf, cpu_info[0], cpu_info[1], cpu_info[2], cpu_info[3]);
-#endif
-}
-
-
-static inline void
 cpuid_serialize(volatile uint32_t *aux)
 {
-    uint32_t cpu_info[4];
-    cpuid(0, cpu_info);
-    *aux = cpu_info[0];
+    union cpu_info cpu_info;
+    cpuid(0, &cpu_info);
+    *aux = cpu_info.regs.eax;
 }
 
 
-static void
-tsc_clock_warmup(struct ubench_clock *clock)
+static int
+is_sse2_supported(void)
 {
-    uint32_t ui, aux;
+    union cpu_info cpu_info;
 
-    (void)clock;
+    cpuid(0x0, &cpu_info);
+    if (cpu_info.regs.eax < 0x1)
+    {
+        return 0;
+    }
 
-    cpuid_serialize(&aux);
-    (void)__rdtsc();
+    cpuid(0x1, &cpu_info);
+    return !!(cpu_info.regs.edx & CPUID_BIT_SSE2);
+}
 
-    (void)__rdtscp(&ui);
-    cpuid_serialize(&aux);
 
-    cpuid_serialize(&aux);
-    (void)__rdtsc();
+static int
+is_rdtscp_supported(void)
+{
+    union cpu_info cpu_info;
 
-    (void)__rdtscp(&ui);
-    cpuid_serialize(&aux);
+    cpuid(0x80000000, &cpu_info);
+    if (cpu_info.regs.eax < 0x80000001)
+    {
+        return 0;
+    }
+
+    cpuid(0x80000001, &cpu_info);
+    return !!(cpu_info.regs.edx & CPUID_BIT_RDTSCP);
 }
 
 
 static ubench_time_t
-tsc_clock_start(struct ubench_clock *clock)
+tsc_clock_start_with_sse2(struct ubench_clock *clock)
+{
+    ubench_time_t start_time;
+
+    (void)clock;
+
+    _mm_mfence();
+    _mm_lfence();
+    start_time = (ubench_time_t)__rdtsc();
+    _mm_lfence();
+
+    return start_time;
+}
+
+
+static ubench_time_t
+tsc_clock_start_without_sse2(struct ubench_clock *clock)
 {
     uint32_t aux;
 
@@ -118,7 +120,28 @@ tsc_clock_start(struct ubench_clock *clock)
 
 
 static int64_t
-tsc_clock_stop(struct ubench_clock *clock, ubench_time_t start_time)
+tsc_clock_stop_with_rdtscp_with_sse2(
+    struct ubench_clock *clock,
+    ubench_time_t start_time
+)
+{
+    ubench_time_t end_time;
+    uint32_t      ui;
+
+    (void)clock;
+
+    end_time = (ubench_time_t)__rdtscp(&ui);
+    _mm_lfence();
+
+    return ubench_time_diff(end_time, start_time);
+}
+
+
+static int64_t
+tsc_clock_stop_with_rdtscp_without_sse2(
+    struct ubench_clock *clock,
+    ubench_time_t start_time
+)
 {
     ubench_time_t end_time;
     uint32_t      ui, aux;
@@ -132,18 +155,96 @@ tsc_clock_stop(struct ubench_clock *clock, ubench_time_t start_time)
 }
 
 
-static void
-tsc_clock_init(struct ubench_clock *clock)
+static int64_t
+tsc_clock_stop_without_rdtscp_with_sse2(
+    struct ubench_clock *clock,
+    ubench_time_t start_time
+)
 {
-    clock->clock_warmup = tsc_clock_warmup;
-    clock->clock_start = tsc_clock_start;
-    clock->clock_stop = tsc_clock_stop;
+    ubench_time_t end_time;
+
+    (void)clock;
+
+    _mm_lfence();
+    end_time = (ubench_time_t)__rdtsc();
+    _mm_lfence();
+
+    return ubench_time_diff(end_time, start_time);
 }
 
 
-const struct ubench_clock_cls x86_tsc_clock_cls =
+static int64_t
+tsc_clock_stop_without_rdtscp_without_sse2(
+    struct ubench_clock *clock,
+    ubench_time_t start_time
+)
+{
+    ubench_time_t end_time;
+    uint32_t      aux;
+
+    (void)clock;
+
+    cpuid_serialize(&aux);
+    end_time = (ubench_time_t)__rdtsc();
+    cpuid_serialize(&aux);
+
+    return ubench_time_diff(end_time, start_time);
+}
+
+
+static void
+tsc_clock_warmup(struct ubench_clock *clock)
+{
+    clock->clock_stop(clock, clock->clock_start(clock));
+    clock->clock_stop(clock, clock->clock_start(clock));
+}
+
+
+static void
+tsc_clock_init(struct ubench_clock *clock)
+{
+    int has_sse2, has_rdtscp;
+
+    has_sse2 = is_sse2_supported();
+    has_rdtscp = is_rdtscp_supported();
+
+    if (has_sse2)
+    {
+        clock->clock_start = tsc_clock_start_with_sse2;
+
+        if (has_rdtscp)
+        {
+            clock->clock_stop = tsc_clock_stop_with_rdtscp_with_sse2;
+        }
+        else
+        {
+            clock->clock_stop = tsc_clock_stop_without_rdtscp_with_sse2;
+        }
+    }
+    else
+    {
+        clock->clock_start = tsc_clock_start_without_sse2;
+
+        if (has_rdtscp)
+        {
+            clock->clock_stop = tsc_clock_stop_with_rdtscp_without_sse2;
+        }
+        else
+        {
+            clock->clock_stop = tsc_clock_stop_without_rdtscp_without_sse2;
+        }
+    }
+
+    clock->clock_warmup = tsc_clock_warmup;
+}
+
+
+const struct ubench_clock_cls ubench_arch_x86_tsc_clock_cls =
 {
     .clock_init = tsc_clock_init,
 };
+
+
+const struct ubench_clock_cls * const ubench_cycle_clock_cls = &ubench_arch_x86_tsc_clock_cls;
 
 #endif /* defined(__i386__) || defined(__x86_64__) */
